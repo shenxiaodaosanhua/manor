f4120c7e451f3278880da7e47bd28ccd28ed3d50
彭嘉良 <87976956@qq.com>
2020-07-01 09:45:25 +0800

    优化代码

--- app/Traits/ErpRequestTrait.php
+++ app/Traits/ErpRequestTrait.php
@@ -16,38 +16,20 @@ use Illuminate\Support\Str;
 trait ErpRequestTrait
 {
     /**
-     * 发送erp POST请求
      * @param string $uri
      * @param array $data
      * @param array $header
      * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function post(string $uri, array $data, array $header = [])
     {
-        $time = time();
-
-        $commonData = [
-            'lang'         => 'zh-cn',
-            'rand_str'     => md5(Str::random(32)),
-            'request_time' => $time,
-        ];
-
-        $data = array_merge($data, $commonData);
-        $data['sign'] = Crypt::signData($data);
-
-        $headerOrigin = [
-            'H-REQUEST-TIME' => $time,
-            'H-APPKEY' => config('erp.app_key'),
-            'H-APPID' => config('erp.app_id'),
-            'H-VERSION' => '1.0.0',
-            'H-SIGN' => $data['sign'],
-        ];
-        $headers = array_merge($headerOrigin, $header);
+        $requestData = $this->handleRequestData($data, $header);
 
         $client = new Client();
         $response = $client->request('post', $uri, [
-            'headers' => $headers,
-            'form_params' => $data,
+            'headers' => $requestData['header'],
+            'form_params' => $requestData['data'],
         ]);
 
         if ($response->getStatusCode() != 200) {
@@ -70,8 +52,40 @@ trait ErpRequestTrait
      * @param array $data
      * @param array $header
      * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function get(string $uri, array $data, array $header = [])
+    {
+
+        $requestData = $this->handleRequestData($data, $header);
+
+        $client = new Client();
+        $response = $client->request('get', $uri, [
+            'headers' => $requestData['header'],
+            'query' => $requestData['data'],
+        ]);
+
+        if ($response->getStatusCode() != 200) {
+            throw new HttpException($response->getBody()->getContents(), $response->getStatusCode());
+        }
+
+        $content = $response->getBody()->getContents();
+        $json = Crypt::decrypt($content);
+        $data = json_decode($json, true);
+
+        if ($data['code'] != 20000) {
+            throw new HttpException($data['msg'], 502);
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param array $data
+     * @param array $header
+     * @return array
+     */
+    protected function handleRequestData(array $data, array $header)
     {
         $time = time();
 
@@ -93,24 +107,9 @@ trait ErpRequestTrait
         ];
         $headers = array_merge($headerOrigin, $header);
 
-        $client = new Client();
-        $response = $client->request('get', $uri, [
-            'headers' => $headers,
-            'query' => $data,
-        ]);
-
-        if ($response->getStatusCode() != 200) {
-            throw new HttpException($response->getBody()->getContents(), $response->getStatusCode());
-        }
-
-        $content = $response->getBody()->getContents();
-        $json = Crypt::decrypt($content);
-        $data = json_decode($json, true);
-
-        if ($data['code'] != 20000) {
-            throw new HttpException($data['msg'], 502);
-        }
-
-        return $data;
+        return [
+            'data' => $data,
+            'header' => $headers,
+        ];
     }
 }
